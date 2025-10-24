ac8076e36e54f266c817d835314e7d2a79338d10
Arnav29X <94759225+Arnav29X@users.noreply.github.com>
2025-10-24 12:16:09 +0530

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <math.h>
 
+int main(){
+    int number, n_copy, count = 0, remainder;
+    double sum = 0;
+
+    printf("Enter to check if the number is ARMSTRONG : ");
+    scanf("%d", &number);
+
+    n_copy = number;
+
+    while (n_copy != 0)
+    {
+        n_copy /= 10;
+        count++;
+    }
+    
+    n_copy = number;
+
+    while (n_copy != 0)
+    {
+        remainder = n_copy % 10;
+        sum += pow(remainder,count);
+        n_copy /= 10;
+    }
+
+    (number == round(sum)) ? printf("ARMSTRONG IT IS !\n") : printf("NOT AN ARMSTRONG !\n");
+    
+    return 0;
+}
