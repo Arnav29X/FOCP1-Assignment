1387c497aef876243c3b72716021c8e7aea2e914
Arnav Verma <94759225+Arnav29X@users.noreply.github.com>
2025-10-28 09:27:53 +0530

    Create Q9.c

---
+++ Q9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int n, i = 0;
+
+    printf("Enter how many elements you want in this array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (i; i < n; i++) {
+        printf("Enter element at index[%d]: ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n; i++) {
+        if (arr[i] == 99) break;
+        else return 1;
+    }
+
+    printf("99's first occurence in this array is at index %d or position %d", i, i+1);
+
+    return 0;
+}
