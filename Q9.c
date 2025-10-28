5835764c033f76e2c678946b0ca6ef06c44c1985
Arnav Verma <94759225+Arnav29X@users.noreply.github.com>
2025-10-28 09:48:35 +0530

    Update Q9.c

--- Q9.c
+++ Q9.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 
-int main(){
-    int n, i = 0;
+int main() {
+    int n, i, found = 0;
 
     printf("Enter how many elements you want in this array: ");
     scanf("%d", &n);
 
     int arr[n];
 
-    for (i; i < n; i++) {
+    for (i = 0; i < n; i++) {
         printf("Enter element at index[%d]: ", i);
         scanf("%d", &arr[i]);
     }
 
     for (i = 0; i < n; i++) {
-        if (arr[i] == 99) break;
-        else {
-            return printf("Array does not contain 99!");
+        if (arr[i] == 99) {
+            found = 1;
+            break;
         }
     }
 
-    printf("99's first occurence in this array is at index %d or position %d", i, i+1);
+    if (found)
+        printf("99's first occurrence is at index %d or position %d\n", i, i + 1);
+    else
+        printf("Array does not contain 99!\n");
 
     return 0;
 }
